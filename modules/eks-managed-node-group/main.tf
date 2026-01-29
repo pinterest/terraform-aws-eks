c981be32ee02ce589b5b3e8badaadf5a89f1e60c
Juan Marino <6354880+TaiSHiNet@users.noreply.github.com>
2026-01-29 15:18:38 -0800

    --wip-- [skip ci]

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -551,6 +551,9 @@ resource "aws_eks_node_group" "this" {
 
   lifecycle {
     create_before_destroy = true
+    ignore_changes = [
+      scaling_config[0].desired_size,
+    ]
   }
 
   tags = merge(
