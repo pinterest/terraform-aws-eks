0794220fd7402c09c540cbc1848e9494c6b8850a
Juan Marino <6354880+TaiSHiNet@users.noreply.github.com>
2026-02-20 12:39:26 -0800

    rely on setting var.use_latest_ami_release_version to false on MNG

--- modules/eks-managed-node-group/main.tf
+++ modules/eks-managed-node-group/main.tf
@@ -427,8 +427,7 @@ locals {
 }
 
 data "aws_ssm_parameter" "ami" {
-  # Skip SSM lookup for CUSTOM ami types or when ami_type is null (custom AMI ID specified)
-  count = var.create && var.use_latest_ami_release_version && var.ami_type != null && var.ami_type != "CUSTOM" ? 1 : 0
+  count = var.create && var.use_latest_ami_release_version ? 1 : 0
 
   region = var.region
 
